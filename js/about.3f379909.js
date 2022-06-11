3e315e1932fc715c77dfcd304ee2c5a7ae1b7565
CI <CI@example.com>
2022-06-11 15:38:17 +0000

    deploy

---
+++ js/about.3f379909.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkview_tube"]=self["webpackChunkview_tube"]||[]).push([[443],{7381:function(e,n,u){u.r(n),u.d(n,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function i(e,n){return(0,t.wg)(),(0,t.iD)("div",a,s)}var c=u(89);const o={},b=(0,c.Z)(o,[["render",i]]);var f=b}}]);
+//# sourceMappingURL=about.3f379909.js.map
